895d95d7334ffb20ffc9ce0cdb52bfa8198dee03
marwan <marwan.elasarey@gamil.com>
2023-02-21 15:46:45 -0800

    New-Task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
