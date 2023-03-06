fc557b2e120da62026e579ec6b412a5aaed69e4e
marwan <123833521+marwan-sam@users.noreply.github.com>
2023-03-06 23:02:59 +0200

    Task Submitted

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,15 +1,9 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
-
-int _putchar(char);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
