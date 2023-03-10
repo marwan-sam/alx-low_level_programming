533fc5dafd697dadcb525800fec6ba438a180ef7
marwan <marwan.elasarey@gamil.com>
2023-03-10 05:25:58 -0800

    File C

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// file c :=
+
+unsigned int _strspn(char *s, char *accept)
+{
+return strspn(s, accept);
+}
