e6984cf8198c1be334c23503741fe2abf1944cd8
marwan <marwan.elasarey@gamil.com>
2023-03-10 05:08:43 -0800

    File C

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// file c :=
+
+char *_strcpy(char *dest, char *src)
+{
+return strcpy(dest, src);
+}
